7447bd8ab8a5c93ccfd5f8444008e090f434566e
kelseyll <kelsey.legault@gmail.com>
2016-07-04 16:08:43 -0700

    Update link color

--- js/main.js
+++ js/main.js
@@ -1,16 +1,30 @@
 $( document ).ready(function() {
 
+    // Define media screen width
+    var mq550 = window.matchMedia( "(min-width: 550px)" );
+
     // Scroll to top on refresh
     $(this).scrollTop(0);
 
     // Print out scroll values
     console.log($(window).scrollTop());
 
-    // Fade elements on scroll
-    $(window).scroll(function(){
-        $(".hero-fixed").css("opacity", 1 - $(window).scrollTop() / 300);
-        $(".hero-relative").css("opacity", 1 - $(window).scrollTop() / 300);
-        $(".description p").css("opacity", 0 + ( $(window).scrollTop() / 500) );
-        $(".bg-fader").css("opacity", 1 - ( $(window).scrollTop() / 200) );
-    });
+    // Fade background and elements on scroll if screen is wider than 550px
+    if (mq550.matches) {
+        $(window).scroll(function(){
+            $(".hero-fixed").css("opacity", 1 - $(window).scrollTop() / 300);
+            $(".hero-relative").css("opacity", 1 - $(window).scrollTop() / 300);
+            $(".description p").css("opacity", 0 + ( $(window).scrollTop() / 500) );
+            $(".bg-fader").css("opacity", 1 - ( $(window).scrollTop() / 200) );
+        });
+    }
+    // Only fade elements on scroll if screen is less than 550px
+    else if (!mq550.matches) {
+        $(window).scroll(function(){
+            $(".hero-relative").css("opacity", 1 - $(window).scrollTop() / 300);
+            $(".hero-fixed").css("opacity", 1 - $(window).scrollTop() / 300);
+            $(".description p").css("opacity", 0 + ( $(window).scrollTop() / 500) );
+        });
+    }
+
 });

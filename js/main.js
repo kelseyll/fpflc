e583cf7790a8cf291724cd9287588e5832ba0d63
kelseyll <kelsey.legault@gmail.com>
2016-07-04 16:33:40 -0700

    Fix mobile

--- js/main.js
+++ js/main.js
@@ -21,10 +21,8 @@ $( document ).ready(function() {
     // Only fade elements on scroll if screen is less than 550px
     else if (!mq550.matches) {
         $(window).scroll(function(){
-            $(".hero-relative").css("opacity", 1 - $(window).scrollTop() / 300);
             $(".hero-fixed").css("opacity", 1 - $(window).scrollTop() / 300);
             $(".description p").css("opacity", 0 + ( $(window).scrollTop() / 500) );
         });
     }
-
 });
